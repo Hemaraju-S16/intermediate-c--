49829a9c05d0e4f0667c0591e719349e0c69722b
Hemaraju S <95607299+Hemaraju-S16@users.noreply.github.com>
2023-02-01 22:06:52 +0530

    new files

--- c++ files/functions.cpp
+++ c++ files/functions.cpp
@@ -14,15 +14,45 @@ void stticTest ()
     cout << "i is " << i << "j is " << j << endl;
 }
 
+void arrayAsparam(int arr[])
+{
+    int marksScored[] = {4,34,5};
+
+    cout << marksScored[1];
+}
+
+void MulArray()
+{
+    int matrixss [2] [2] = {
+        {1,2}, {4,5}
+    };
+}
+
+void myarray()
+{
+    int mularray [3] [3]{  
+        {2, 5, 5},  
+        {4, 0, 3},  
+        {9, 1, 8}  
+        };
+
+
+    for (int x = 0; x < 3; x++)
+    {
+        for(int y = 0; y < 3; y++)
+        {
+            cout << mularray[x][y] << " ";
+        }
+        cout << ""<< endl;
+    }
+}
+
 
 
 int main()
-{
+{   
+    myarray();
 
-    
-    stticTest();
-    stticTest();
-    stticTest();
     return 0;
 }
 
---
+++ c++ files/pointers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int number = 10;
+
+    int *ptr;
+
+    ptr = &number;
+
+    cout <<ptr;
+
+    return 0;
+
+}
