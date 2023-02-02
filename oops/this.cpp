40a2261cc269a2e16de897245f84b2c9348012a9
Hemaraju S <95607299+Hemaraju-S16@users.noreply.github.com>
2023-02-02 18:12:09 +0530

    all files

---
+++ oops/this.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+struct stud {
+        int age;
+        int marks;
+        string names;
+
+        stud (int i, string m)
+        {
+            age = i;
+            names = m;
+        }
+
+        void display()
+        {
+            cout <<" age is " <<age <<" and name is " << names; 
+        }
+    };
+
+
+int main()
+
+{
+
+    /*stud s1;
+    stud s2;
+    
+    s1.age = 12;
+    s1.marks = 45;
+    s1.names  = "hem";
+
+    s2.age = 45;
+    s2.marks = 89;
+    s2.names = "raju";
+
+    cout << s2.age;*/
+
+
+    stud valid = stud (12,"maj");
+
+    valid.marks = 45;
+
+    valid.display();
+
+    return 0;
+    
+}
