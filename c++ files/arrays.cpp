ed7dda0ebb04a2dd60fa642eaef2ef1456125034
Hemaraju S <95607299+Hemaraju-S16@users.noreply.github.com>
2023-02-02 12:02:51 +0530

    asdf

--- c++ files/arrays.cpp
+++ c++ files/arrays.cpp
@@ -3,17 +3,12 @@ using namespace std;
 
 int main()
 {
-    int marks[] = {1,2,4,5,67,89,90};
+   int mulArrays[2][1] = { 
+    {1}, {2} };
 
-    int total;
+   cout <<mulArrays[1][0];
 
-
-    for(int x : marks)
-    {
-        total += x;
-    }
-
-    cout << "sum is " << total;
-    return 0;
+   return 0;
+    
 
 }
